5020d5e6fd52c256dffd60dcb07fef6a32fa6953
vitmvit <120332119+vitmvit@users.noreply.github.com>
2024-04-06 00:34:08 +0300

    refactor

---
+++ src/main/java/by/vitikova/discovery/SpringCloudServiceApplication.java
@@ -0,0 +1,14 @@
+package by.vitikova.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudServiceApplication.class, args);
+    }
+}
